a76eae4a458532413d482250d60ad8c356647704
Seth Vargo <sethvargo@gmail.com>
2013-06-14 22:50:07 -0600

    Fix metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'myface'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Seth Vargo'
+maintainer_email 'sethvargo@opscode.com'
 license          'All rights reserved'
 description      'Installs/Configures myface'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
